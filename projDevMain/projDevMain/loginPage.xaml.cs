8c4d7dc926c44b3e4b5d80620011a4f6753e96d1
Joey Boy Mission <joeyboy.mission@tup.edu.ph>
2024-06-01 00:00:44 +0800

    Initial Commit Project Development
    NEWLY UPDATED
    XAML
    - Login Page
    - Register Page
    
    CODEBHIND
    - No new update yet

---
+++ projDevMain/projDevMain/loginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace projDevMain
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class loginPage : ContentPage
+	{
+		public loginPage ()
+		{
+			InitializeComponent ();
+
+			//NAVIGATION PAGE PROPERTIES
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void RegisterPage_Tapped(object sender, EventArgs e)
+        {
+			Navigation.PushAsync(new registerPage());
+        }
+    }
+}
