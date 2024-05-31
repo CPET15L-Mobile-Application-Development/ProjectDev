2d2163dac70fdd320c4dc41e496692dade96cc72
Joey Boy Mission <joeyboy.mission@tup.edu.ph>
2024-05-31 20:33:19 +0800

    Initial Commit Project Development
    - Initial base of the code
    - No GUI, can be developed further
    - No code behind function

---
+++ MissionPancho_ProjDev/MissionPancho_ProjDev/homePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MissionPancho_ProjDev
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class homePage : ContentPage
+    {
+        public homePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
